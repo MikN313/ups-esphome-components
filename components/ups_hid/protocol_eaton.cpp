848445ab9e7cc0317f319b8b5a256c6531bc83cf
MikN313 <mik313var@gmail.com>
2026-08-06 16:21:17 +0200

    Update protocol_eaton.cpp

--- components/ups_hid/protocol_eaton.cpp
+++ components/ups_hid/protocol_eaton.cpp
@@ -1467,12 +1467,18 @@ std::unique_ptr<UpsProtocolBase> create_eaton_protocol(UpsHidComponent* parent) 
 } // namespace ups_hid
 } // namespace esphome
 
-// Register Eaton protocol for vendor ID 0x0463
-//REGISTER_UPS_PROTOCOL_FOR_VENDOR(0x0463, eaton_hid_protocol, esphome::ups_hid::create_eaton_protocol, "Eaton HID Protocol", "Eaton HID protocol with comprehensive sensor support and test functionality", 100);
-REGISTER_UPS_PROTOCOL_FOR_VENDOR(
-    0x0463,
-    eaton,
-    esphome::ups_hid::create_eaton_protocol,
-    "Eaton HID Protocol",
-    "Eaton HID protocol with comprehensive sensor support and test functionality",
-    100);
+namespace {
+struct EatonProtocolRegistration {
+  EatonProtocolRegistration() {
+    ProtocolFactory::ProtocolInfo info;
+    info.name = "eaton";
+    info.priority = 100;
+    info.creator = [](UpsHidComponent *parent) -> std::unique_ptr<UpsProtocolBase> {
+      return create_eaton_protocol(parent);
+    };
+    ProtocolFactory::register_protocol_for_vendor(0x0463, info);
+  }
+};
+
+static EatonProtocolRegistration eaton_protocol_registration;
+}  // namespace
